b544b287d4f1131f341f601e9a96411783828a18
Chris Aquino <chris.aquino@gmail.com>
2018-12-07 14:02:22 -0500

    styles the textarea

--- src/EditorWindow.js
+++ src/EditorWindow.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 const EditorWindow = (props) => {
     return (
-      <div className="editor-window">
-        it was the best of times, it was the pretty okish of times.
-      </div>
+
+        <textarea 
+        className="form-control editor-window" 
+        rows="3" 
+        value="it was the best of times, it was the pretty okish of times."
+        />
 
     );
 };
